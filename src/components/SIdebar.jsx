869ea9f0f71037dc8fee7482c0e71801b4a46508
itzzsauravp <social.saurav2003@gmail.com>
2024-07-28 14:54:09 +0545

    added NavLink

--- src/components/SIdebar.jsx
+++ src/components/SIdebar.jsx
@@ -1,44 +1,31 @@
+import { NavLink } from "react-router-dom";
 import Footer from "./Footer";
 
-const Sidebar = ({ selectedTab, toggleSelectedTab }) => {
-  const handleSelectedTab = (e) => {
-    toggleSelectedTab(e.target.textContent);
-  };
+const Sidebar = () => {
   return (
     <div
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
       style={{ width: "280px" }}
     >
-      <a
-        href="/"
+      <NavLink
+        to="/"
         className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
       >
         <span className="fs-4">
           <strike>Facebook</strike> Notebook
         </span>
-      </a>
+      </NavLink>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        <li className="nav-item" onClick={handleSelectedTab}>
-          <a
-            href="#"
-            className={`nav-link text-white ${
-              selectedTab === "Home" && "active"
-            }`}
-            aria-current="page"
-          >
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link text-white" aria-current="page">
             Home
-          </a>
+          </NavLink>
         </li>
-        <li onClick={handleSelectedTab}>
-          <a
-            href="#"
-            className={`nav-link text-white ${
-              selectedTab === "Create Post" && "active"
-            }`}
-          >
+        <li>
+          <NavLink to="/create-post" className="nav-link text-white">
             Create Post
-          </a>
+          </NavLink>
         </li>
       </ul>
       <div>

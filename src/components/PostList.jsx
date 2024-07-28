a31383525add672f627561a1e5f7733d10586691
itzzsauravp <social.saurav2003@gmail.com>
2024-07-28 14:53:32 +0545

    added header component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,18 @@
-import Post from "./Post";
-import { useContext } from "react";
 import { PostList as PostListContext } from "../store/post-list-store";
-const PostList = ({toggleSelectedTab}) => {
+import { useContext } from "react";
+import Post from "./Post";
+import Header from "./Header";
+
+const PostList = () => {
   const { postList } = useContext(PostListContext);
   return (
     <div className="post-container">
-      {postList.map((post) => (
-        <Post key={post.id} post={post} toggleSelectedTab={toggleSelectedTab} />
-      ))}
+      <Header />
+      <div>
+        {postList.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
     </div>
   );
 };
